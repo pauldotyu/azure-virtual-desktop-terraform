aef5b0629e391e6d314fdb6a44f2a8de2c12791e
Paul Yu <pauldotyu@gmail.com>
2021-10-07 17:37:58 -0700

    conditionally configure using ansible or azure

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ data "azurerm_resources" "vnets" {
   }
 }
 
-// this will peer out to all the virtual networks tagged with a role of azops
+# this will peer out to all the virtual networks tagged with a role of azops
 resource "azurerm_virtual_network_peering" "out" {
   count                        = length(data.azurerm_resources.vnets.resources)
   name                         = "${azurerm_virtual_network.avd.name}-to-${data.azurerm_resources.vnets.resources[count.index].name}"
@@ -71,8 +71,9 @@ resource "azurerm_virtual_network_peering" "out" {
   use_remote_gateways          = false
 }
 
-// this will peer in from all the virtual networks tagged with the role of azops
-// this also needs work. right now it is using variables when it should be using the data resource pulled from above; howver, the challenge is that the data resource does not return the resrouces' resource group which is required for peering
+# this will peer in from all the virtual networks tagged with the role of azops
+# this also needs work. right now it is using variables when it should be using the data resource pulled from above;
+# howver, the challenge is that the data resource does not return the resrouces' resource group which is required for peering
 resource "azurerm_virtual_network_peering" "in" {
   for_each                     = { for vp in var.vnet_peerings : vp.vnet_name => vp }
   name                         = "${each.value["vnet_name"]}-to-${azurerm_virtual_network.avd.name}"
@@ -106,8 +107,7 @@ resource "azurerm_virtual_desktop_host_pool" "avd" {
   }
 }
 
-# Both desktop and remote app application groups are being joined to a single host pool
-# however, in production scenario, these should join to separate host pools
+# Both desktop and remote app application groups are being joined to a single host pool however, in production scenario, these should join to separate host pools
 
 resource "azurerm_virtual_desktop_application_group" "avd" {
   name                = "ag-${random_pet.avd.id}"
@@ -134,14 +134,51 @@ resource "azurerm_virtual_desktop_workspace_application_group_association" "avd"
 # AZURE VIRTUAL DESKTOP - SESSION HOSTS
 ##############################################
 
-# Make sure the VM name prefix doesn't exceed 12 characters
+data "azurerm_shared_image" "avd" {
+  name                = var.sig_image_name
+  gallery_name        = var.sig_name
+  resource_group_name = var.sig_resource_group_name
+}
+
+data "azurerm_shared_image_version" "avd" {
+  name                = "latest"
+  image_name          = var.sig_image_name
+  gallery_name        = var.sig_name
+  resource_group_name = var.sig_resource_group_name
+}
+
+resource "random_string" "avd" {
+  length  = 3
+  upper   = true
+  number  = true
+  lower   = false
+  special = false
+
+  # this ensures that we get a fresh set of VMs on every run
+  keepers = {
+    timestamp = timestamp()
+  }
+}
+
 locals {
-  vm_name = substr(format("vm%s", random_pet.avd.id), 0, 12)
+  # NOTE: this algorithm works for images that were built using Azure Image Builder as they produce unique image versions that look like this: 0.24926.27922
+  # Logic to build VM name prefix:
+  # 1. Get the image version
+  # 2. Grab the id
+  # 3. Split the id into an array using slash
+  # 4. Reverse the order of the elements in the array
+  # 5. Take the first element
+  # 6. Remove the dot
+  # vm_name_prefix = replace(element(reverse(split("/", data.azurerm_shared_image_version.avd.id)), 0), ".", "")
+  # vm_image_name  = element(reverse(split("/", data.azurerm_shared_image_version.avd.id)), 2)
+
+  vm_name_prefix = format("VM%s%s", upper(substr(random_pet.avd.id, 0, 7)), random_string.avd.result)
+  vm_image_name  = var.vm_image.sku
 }
 
 resource "azurerm_network_interface" "avd" {
   count               = var.vm_count
-  name                = "${local.vm_name}-${count.index + 1}_nic"
+  name                = "${local.vm_name_prefix}-${count.index + 1}_nic"
   resource_group_name = azurerm_resource_group.avd.name
   location            = azurerm_resource_group.avd.location
 
@@ -151,55 +188,57 @@ resource "azurerm_network_interface" "avd" {
     private_ip_address_allocation = "dynamic"
   }
 
+  # lifecycle {
+  #   create_before_destroy = true
+  # }
+
   tags = var.tags
 }
 
-data "azurerm_shared_image" "avd" {
-  name                = var.sig_image_name
-  gallery_name        = var.sig_name
-  resource_group_name = var.sig_resource_group_name
-}
-
-# data "azurerm_shared_image_version" "avd" {
-#   name                = "latest"
-#   image_name          = var.sig_image_name
-#   gallery_name        = var.sig_name
-#   resource_group_name = var.sig_resource_group_name
-# }
-
 resource "azurerm_windows_virtual_machine" "avd" {
-  count               = var.vm_count
-  name                = "${local.vm_name}-${count.index + 1}"
-  resource_group_name = azurerm_resource_group.avd.name
-  location            = azurerm_resource_group.avd.location
-  size                = var.vm_sku
-  admin_username      = var.username
-  admin_password      = var.password
-  tags                = merge(var.tags, { "role" = "AVDSessionHost" })
+  count                    = var.vm_count
+  name                     = "${local.vm_name_prefix}-${count.index + 1}"
+  resource_group_name      = azurerm_resource_group.avd.name
+  location                 = azurerm_resource_group.avd.location
+  size                     = var.vm_sku
+  admin_username           = var.username
+  admin_password           = var.password
+  enable_automatic_updates = false
+  patch_mode               = "Manual"
+  license_type             = "Windows_Client"
+  timezone                 = "Pacific Standard Time"
+
+  tags = merge(var.tags, {
+    "role"  = "AVDSessionHost"
+    "image" = local.vm_image_name
+  })
 
   network_interface_ids = [
     element(azurerm_network_interface.avd.*.id, count.index)
   ]
 
   os_disk {
-    name                 = "${local.vm_name}-${count.index + 1}_osdisk"
+    name                 = "${local.vm_name_prefix}-${count.index + 1}_osdisk"
     caching              = var.vm_os_disk_caching.caching
     storage_account_type = var.vm_os_disk_caching.storage_account_type
   }
 
-  # source_image_reference {
-  #   publisher = var.vm_image.publisher
-  #   offer     = var.vm_image.offer
-  #   sku       = var.vm_image.sku
-  #   version   = var.vm_image.version
-  # }
+  source_image_reference {
+    publisher = var.vm_image.publisher
+    offer     = var.vm_image.offer
+    sku       = var.vm_image.sku
+    version   = var.vm_image.version
+  }
 
-  source_image_id = data.azurerm_shared_image.avd.id
-  #source_image_id = data.azurerm_shared_image_version.avd.id
+  # source_image_id = data.azurerm_shared_image.avd.id
 
   identity {
     type = "SystemAssigned"
   }
+
+  # lifecycle {
+  #   create_before_destroy = true
+  # }
 
   depends_on = [
     azurerm_virtual_network_peering.out,
@@ -207,62 +246,66 @@ resource "azurerm_windows_virtual_machine" "avd" {
   ]
 }
 
-##################################################################
-# AZURE VIRTUAL DESKTOP - SESSION HOST ANSIBLE CONFIGURATION
-##################################################################
+###############################################################################################
+# AZURE VIRTUAL DESKTOP - SESSION HOST ANSIBLE CONFIGURATION  - IF USING ANSIBLE TO CONFIGURE
+###############################################################################################
 
 # Install WinRM for Ansible
 # https://docs.microsoft.com/en-us/azure/virtual-machines/extensions/custom-script-windows
-# resource "azurerm_virtual_machine_extension" "avd" {
-#   count                      = length(azurerm_windows_virtual_machine.avd)
-#   name                       = "AnsibleWinRM"
-#   virtual_machine_id         = azurerm_windows_virtual_machine.avd[count.index].id
-#   publisher                  = "Microsoft.Compute"
-#   type                       = "CustomScriptExtension"
-#   type_handler_version       = "1.10"
-#   auto_upgrade_minor_version = true
+resource "azurerm_virtual_machine_extension" "avd" {
+  count                      = var.configure_using_ansible ? length(azurerm_windows_virtual_machine.avd) : 0
+  name                       = "AnsibleWinRM"
+  virtual_machine_id         = azurerm_windows_virtual_machine.avd[count.index].id
+  publisher                  = "Microsoft.Compute"
+  type                       = "CustomScriptExtension"
+  type_handler_version       = "1.10"
+  auto_upgrade_minor_version = true
 
-#   settings = <<SETTINGS
-#     {
-#       "fileUris": [
-#           "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
-#         ]
-#     }
-#   SETTINGS
+  settings = <<SETTINGS
+    {
+      "fileUris": [
+          "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
+        ]
+    }
+  SETTINGS
 
-#   protected_settings = <<PROTECTED_SETTINGS
-#     {
-#       "commandToExecute": "powershell.exe -ExecutionPolicy Bypass -Command \"./ConfigureRemotingForAnsible.ps1 -EnableCredSSP -DisableBasicAuth -Verbose; exit 0;\""
-#     }
-#   PROTECTED_SETTINGS
+  protected_settings = <<PROTECTED_SETTINGS
+    {
+      "commandToExecute": "powershell.exe -ExecutionPolicy Bypass -Command \"./ConfigureRemotingForAnsible.ps1 -EnableCredSSP -DisableBasicAuth -Verbose; exit 0;\""
+    }
+  PROTECTED_SETTINGS
 
-#   depends_on = [
-#     azurerm_virtual_network_peering.out,
-#     azurerm_virtual_network_peering.in
-#   ]
-# }
+  # lifecycle {
+  #   create_before_destroy = true
+  # }
 
-# ################################
-# # BUILD ANSIBLE INVENTORY
-# ################################
+  depends_on = [
+    azurerm_virtual_network_peering.out,
+    azurerm_virtual_network_peering.in
+  ]
+}
 
-# resource "local_file" "avd" {
-#   filename = "ansible/inventory"
+############################################################
+# BUILD ANSIBLE INVENTORY - IF USING ANSIBLE TO CONFIGURE
+###########################################################
 
-#   content = templatefile("ansible/template-inventory.tpl",
-#     {
-#       hosts = zipmap(azurerm_windows_virtual_machine.avd.*.name, azurerm_network_interface.avd.*.private_ip_address),
-#     }
-#   )
-# }
+resource "local_file" "avd" {
+  count    = var.configure_using_ansible ? 1 : 0
+  filename = "ansible/inventory"
+  content = templatefile("ansible/template-inventory.tpl",
+    {
+      hosts = zipmap(azurerm_windows_virtual_machine.avd.*.name, azurerm_network_interface.avd.*.private_ip_address),
+    }
+  )
+}
 
-################################
-# DOMAIN JOIN EXTENSION
-################################
+#############################################################
+# DOMAIN JOIN EXTENSION - IF NOT USING ANSIBLE TO CONFIGURE
+#############################################################
 
 resource "azurerm_virtual_machine_extension" "domain_join" {
-  count                      = var.vm_count
-  name                       = "${local.vm_name}-${count.index + 1}-domainJoin"
+  count                      = var.configure_using_ansible ? 0 : var.vm_count
+  name                       = "${local.vm_name_prefix}-${count.index + 1}-domainJoin"
   virtual_machine_id         = azurerm_windows_virtual_machine.avd.*.id[count.index]
   publisher                  = "Microsoft.Compute"
   type                       = "JsonADDomainExtension"
@@ -286,7 +329,8 @@ SETTINGS
 PROTECTED_SETTINGS
 
   lifecycle {
-    ignore_changes = [settings, protected_settings]
+    # create_before_destroy = true
+    ignore_changes        = [settings, protected_settings]
   }
 
   depends_on = [
@@ -295,13 +339,13 @@ PROTECTED_SETTINGS
   ]
 }
 
-################################
-# AVD AGENT INSTALL EXTENSION
-################################
+###################################################################
+# AVD AGENT INSTALL EXTENSION - IF NOT USING ANSIBLE TO CONFIGURE
+###################################################################
 
 resource "azurerm_virtual_machine_extension" "agent_install" {
-  count                      = var.vm_count
-  name                       = "${local.vm_name}-${count.index + 1}-avd_dsc"
+  count                      = var.configure_using_ansible ? 0 : var.vm_count
+  name                       = "${local.vm_name_prefix}-${count.index + 1}-avd_dsc"
   virtual_machine_id         = azurerm_windows_virtual_machine.avd.*.id[count.index]
   publisher                  = "Microsoft.Powershell"
   type                       = "DSC"
@@ -325,6 +369,10 @@ SETTINGS
     }
   }
 PROTECTED_SETTINGS
+
+  # lifecycle {
+  #   create_before_destroy = true
+  # }
 
   depends_on = [
     azurerm_virtual_machine_extension.domain_join,
--- variables.tf
+++ variables.tf
@@ -110,19 +110,33 @@ variable "sig_resource_group_name" {
   description = "Shared Image Gallery resource group name"
 }
 
+variable "configure_using_ansible" {
+  type        = bool
+  description = "Set this to true if you want to use Ansible to perform the domain join and session host agent installation"
+  default     = false
+}
+
+####################################################
+# The variables listed below are only needed
+# if not using Ansible to configure session hosts
+####################################################
 variable "domain_name" {
-  type = string
+  type    = string
+  default = ""
 }
 
 variable "domain_ou_path" {
-  type = string
+  type    = string
+  default = ""
 }
 
 variable "domain_username" {
-  type = string
+  type    = string
+  default = ""
 }
 
 variable "domain_password" {
   type      = string
   sensitive = true
+  default   = ""
 }
